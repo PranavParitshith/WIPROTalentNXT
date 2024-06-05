a328683651783d509ff92af0c702a5d0908a8ae3
PranavParitshith <113255405+PranavParitshith@users.noreply.github.com>
2024-06-05 21:38:31 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,6 @@
+class Third {
+    public static void main(String args[]) {
+        int c = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println("The Sum of " + args[0] + " and " + args[1] + " is " + c);
+    }
+}
